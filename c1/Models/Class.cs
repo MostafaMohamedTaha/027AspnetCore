b18e8b2354ddd48e6bd484addb1155610bc183f0
MostafaMohamedTaha <MostafaMohamedTaha.GitHup@gmail.com>
2023-07-11 08:06:39 +0200

    1
    a

---
+++ c1/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace c1.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
+ 
